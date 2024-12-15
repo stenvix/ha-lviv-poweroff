ffd333ef304a1066eab9b694c5cea878c4d14da2
Valentyn Stepanov <falken.ua@gmail.com>
2024-12-16 01:11:31 +0200

    add support for extracting and validating HH:MM time format from periods

--- custom_components/lviv_poweroff/energyua_scrapper.py
+++ custom_components/lviv_poweroff/energyua_scrapper.py
@@ -10,6 +10,7 @@ from .entities import PowerOffPeriod
 
 URL = "https://lviv.energy-ua.info/grupa/{}"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"
+TIMEPATTERN = r'^\d{2}:\d{2}$'
 
 class EnergyUaScrapper:
     """Class for scraping power off periods from the Energy UA website."""
@@ -25,22 +26,22 @@ class EnergyUaScrapper:
         ):
             return response.status == 200
 
-    @staticmethod
-    def merge_periods(periods: list[PowerOffPeriod]) -> list[PowerOffPeriod]:
-        if not periods:
-            return []
+    # @staticmethod
+    # def merge_periods(periods: list[PowerOffPeriod]) -> list[PowerOffPeriod]:
+    #     if not periods:
+    #         return []
 
-        periods.sort(key=lambda x: x.start)
+    #     periods.sort(key=lambda x: x.startHour)
 
-        merged_periods = [periods[0]]
-        for current in periods[1:]:
-            last = merged_periods[-1]
-            if current.start <= last.end:  # Overlapping or contiguous periods
-                last.end = max(last.end, current.end)
-                continue
-            merged_periods.append(current)
+    #     merged_periods = [periods[0]]
+    #     for current in periods[1:]:
+    #         last = merged_periods[-1]
+    #         if current.startHour <= last.endHour:  # Overlapping or contiguous periods
+    #             last.end = max(last.end, current.end)
+    #             continue
+    #         merged_periods.append(current)
 
-        return merged_periods
+    #     return merged_periods
 
     async def get_power_off_periods(self) -> list[PowerOffPeriod]:
         async with (
@@ -50,23 +51,25 @@ class EnergyUaScrapper:
             content = await response.text()
             soup = BeautifulSoup(content, "html.parser")
             results = []
-            scale_hours = soup.find_all("div", class_="scale_hours")
-            if len(scale_hours) > 0:
-                scale_hours_el = scale_hours[0].find_all("div", class_="scale_hours_el")
-                for item in scale_hours_el:
-                    if item.find("span", class_="hour_active"):
-                        start, end = self._parse_item(item)
-                        results.append(PowerOffPeriod(start, end, today=True))
-                results = self.merge_periods(results)
-            if len(scale_hours) > 1:
-                tomorrow_results = []
-                scale_hours_el_tomorrow = scale_hours[1].find_all("div", class_="scale_hours_el")
-                for item in scale_hours_el_tomorrow:
-                    if item.find("span", class_="hour_active"):
-                        start, end = self._parse_item(item)
-                        tomorrow_results.append(PowerOffPeriod(start, end, today=False))
-                results += self.merge_periods(tomorrow_results)
+            for scale_period in soup.find_all("div", class_ = "scale_info_periods"):
+                isToday = True
+                header = scale_period.find("h4", class_ = "scale_info_title")
+                if(re.search('завтра', header.text, re.IGNORECASE)):
+                    isToday = False
 
+                periods = scale_period.find_all("div", class_="periods_items")
+                for period in periods:
+                    for item in period.find_all("span"):
+                        hours = item.find_all("b")
+                        times = []
+                        for hour in hours:
+                            if(bool(re.match(TIMEPATTERN, hour.text))):
+                                print(hour.text)
+                                times.append(hour.text)
+                        if(len(times)==2):
+                            results.append(PowerOffPeriod(int(times[0].split(':')[0]),int(times[0].split(':')[1]),int(times[1].split(':')[0]), int(times[1].split(':')[1]), today=isToday))
+                    # results += self.merge_periods(results)
+            
             return results
 
     def _parse_item(self, item: BeautifulSoup) -> tuple[int, int]:
--- custom_components/lviv_poweroff/entities.py
+++ custom_components/lviv_poweroff/entities.py
@@ -8,8 +8,10 @@ from datetime import datetime, timedelta
 class PowerOffPeriod:
     """Class for power off period."""
 
-    start: int
-    end: int
+    startHour: int
+    startMinute: int
+    endHour: int
+    endMinute: int
     today: bool
 
     def to_datetime_period(self, tz_info) -> tuple[datetime, datetime]:
@@ -18,8 +20,8 @@ class PowerOffPeriod:
         if not self.today:
             now += timedelta(days=1)
 
-        start = now.replace(hour=self.start, minute=0, second=0, microsecond=0)
-        end = now.replace(hour=self.end, minute=0, second=0, microsecond=0)
+        start = now.replace(hour=self.startHour, minute=self.startMinute, second=0, microsecond=0)
+        end = now.replace(hour=self.endHour, minute=self.endMinute, second=0, microsecond=0)
         if end < start:
             end += timedelta(days=1)
         return start, end
